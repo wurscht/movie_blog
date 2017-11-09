66abd0514cec71feba3566253109a621be5c7b1d
wurscht <lehjo@gmx.ch>
2017-11-09 09:33:12 +0100

    show active blog

--- blogs.php
+++ blogs.php
@@ -6,10 +6,21 @@
   // Hier Code....
   if (is_array($blogs) || is_object($blogs)){
     foreach ($blogs as $blog) {
-      echo "<div><a href='index.php?function=blogs&bid=$blogId' title='Blog auswählen'><h2>" . $blog['name'] . "</h2></a></div>";
+      if ($blog['uid'] == $blogId) {
+        echo "<div><h2>" . $blog['name'] . "</h2></div>";
+      } else {
+        echo "<div><a href='index.php?function=blogs&bid=" . $blog['uid'] . "' title='Blog auswählen'><h2>" . $blog['name'] . "</h2></a></div>";
+      }
     }
   }
+  
+  
 
   // Nachfolgend das Beispiel einer Ausgabe in HTML, dieser Teil muss mit einer Schlaufe über alle Blogs und der Ausgabe mit PHP ersetzt werden
 ?>
+	<!-- <div><a href='index.php?function=blogs&bid=4' title='Blog auswählen'><h4>Anna Abegglen</h4></a></div>
+	<div><a href='index.php?function=blogs&bid=2' title='Blog auswählen'><h4>Hans Hinterseer</h4></a></div>
+	<div><a href='index.php?function=blogs&bid=1' title='Blog auswählen'><h4>Marc Muster</h4></a></div>
+	<div><a href='index.php?function=blogs&bid=3' title='Blog auswählen'><h4>Sonja Sauser</h4></a></div> -->
+
 
