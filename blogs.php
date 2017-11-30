a932687b599f9c69520cfd3dc305983b75ca41ed
HavaFuga <32871313+HavaFuga@users.noreply.github.com>
2017-11-30 09:28:26 +0100

    blog verbessert

--- blogs.php
+++ blogs.php
@@ -1,11 +1,9 @@
 <?php
   // Alle Blogs bzw. Benutzernamen holen und falls Blog bereits ausgewählt, entsprechenden Namen markieren
   // Hier Code....
-  $blogs = getUserNames(); ?>
+  $blogs = getUserNames();
   // Schlaufe über alle Blogs bzw. Benutzer
   // Hier Code....
-<div class="chooseB">
-<?php
   if (is_array($blogs) || is_object($blogs)){
     foreach ($blogs as $blog) {
       if ($blog['uid'] == $blogId) {
@@ -16,9 +14,8 @@
     }
   }
 ?>
-
-  // Nachfolgend das Beispiel einer Ausgabe in HTML, dieser Teil muss mit einer Schlaufe über alle Blogs und der Ausgabe mit PHP ersetzt werden
-
+<div class="chooseB">
+  
 	<!-- <div><a href='index.php?function=blogs&bid=4' title='Blog auswählen'><h4>Anna Abegglen</h4></a></div>
 	<div><a href='index.php?function=blogs&bid=2' title='Blog auswählen'><h4>Hans Hinterseer</h4></a></div>
 	<div><a href='index.php?function=blogs&bid=1' title='Blog auswählen'><h4>Marc Muster</h4></a></div>
