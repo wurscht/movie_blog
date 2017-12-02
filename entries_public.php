ffef0fb1d675afa6197540a22aa45f9b29fca461
wurscht <lehjo@gmx.ch>
2017-12-02 13:44:50 +0100

    order choice to the left of the page

--- entries_public.php
+++ entries_public.php
@@ -10,20 +10,39 @@
 
     foreach ($blogs as $blog) {
         if ($blog['uid'] == $blogId) {
+            echo '<div class="blog">';
             echo "<p>" . $blog['name']."</p>";
+            echo '</div>';
         }
     }
 
+    echo '<div class="row">';
+    echo '<div class="col col-md-4">';
+
+    foreach ($entries as $entry){
+        echo '<a href="index.php?function=entries_public&eid="' . $entry['eid'] . '">';
+        echo '<div class="entry" value="' . $entry['eid'] .'">';
+        echo "<h4>".$entry['title'].", ".gmdate("Y.m.d, H:i:s", $entry['datetime'])."</h4><br>";
+        echo nl2br(substr($entry['content'], 0, 100). "...");
+        echo '</div>';
+        echo '</a>';
+    }
+
+    echo '</div>';
+    echo '<div class="col col-md-8">';
+
     if (empty($entries)){
-        echo "<h2>Hoooopla! Keine Blogeinträge gefunden.</h2>";
+        echo "<h2>Hoppla! Keine Blogeinträge gefunden.</h2>";
         }
     else
         foreach ($entries as $entry){
-            echo "<h2>".$entry['title'].", ".gmdate("Y.m.d, H:i:s", $entry['datetime'])."</h2><br>";
-            echo nl2br($entry['content']);
-            echo '<form method="post">';
-            echo '<button type="submit" name="delete-entry" value=' . $entry['eid'] . 'id="delete-entry">Lösche diesen Beitrag</button>';
-            echo '</form>';
+            if (isset($_GET['eid'])){
+                echo "<h2>".$entry['title'].", ".gmdate("Y.m.d, H:i:s", $entry['datetime'])."</h2><br>";
+                echo nl2br($entry['content']);
+                echo '<form method="post">';
+                echo '<button type="submit" name="delete-entry" value=' . $entry['eid'] . 'id="delete-entry">Lösche diesen Beitrag</button>';
+                echo '</form>';
+            }
         } 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['delete-entry'])) {
@@ -31,6 +50,8 @@
             // header("Location: index.php?function=blogs&bid=" . $blogId);
         }
     }
+
+    echo '</div>';
 ?>
 
 <!--include "connection file";
