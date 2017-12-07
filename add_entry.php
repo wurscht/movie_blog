80268016c429c5c0cfb0b3763fe42cde2b44a8e9
wurscht <lehjo@gmx.ch>
2017-12-07 09:30:34 +0100

    add redirect after save

--- add_entry.php
+++ add_entry.php
@@ -1,9 +1,10 @@
 <?php
 
 if (isset($_POST['titel']) and isset($_POST['inhalt'])) {
-    addEntry($userId, $_POST['titel'], $_POST['inhalt']); 
+    addEntry($blogId, $_POST['titel'], $_POST['inhalt']);
+    header("Location: index.php?function=entries_public&bid=" . $blogId);
 }
-
+echo $blogId;
 ?>
 
 <form method="post" action="">
