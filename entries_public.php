6bd42ebda7d52a5e9a7cabdce6252feb006f4f75
wurscht <lehjo@gmx.ch>
2017-12-07 08:48:29 +0100

    fix entries_public

--- entries_public.php
+++ entries_public.php
@@ -7,6 +7,9 @@
 
     $entries = getEntries($blogId);
     $blogs = getUserNames();
+    if (isset($_GET['eid'])) {
+    $eid = $_GET['eid'];
+    }
 
     foreach ($blogs as $blog) {
         if ($blog['uid'] == $blogId) {
@@ -21,8 +24,7 @@
 
     foreach ($entries as $entry){
         if (isset($entry['eid'])){
-        echo '<a href="index.php?function=entries_public&bid=' . $blog['uid'] . '&eid=' . $entry['eid'] . '">';
-        var_dump($entry['eid']);
+        echo '<a href="index.php?function=entries_public&bid=' . $blogId . '&eid=' . $entry['eid'] . '">';
         echo '<div class="entry" value="' . $entry['eid'] .'">';
         echo "<h4>".$entry['title'].", ".gmdate("Y.m.d, H:i:s", $entry['datetime'])."</h4>";
         echo nl2br(substr($entry['content'], 0, 100). "...");
@@ -33,20 +35,22 @@
 
     echo '</div>';
     echo '<div class="col col-sm-8">';
+    
+    
 
-    $shown_entry = getEntry($entry['eid']);
-
-    if (empty($shown_entry)){
+    if (empty($eid)){
         echo "<h2>Hoppla! Keine Blogeinträge gefunden.</h2>";
         }
-    else
-        if (isset($_GET['eid'])){
+    else {
+        if ($eid){
+            $shown_entry = getEntry($eid);
             echo "<h2>".$shown_entry['title'].", ".gmdate("Y.m.d, H:i:s", $shown_entry['datetime'])."</h2>";
             echo nl2br($shown_entry['content']);
             echo '<form method="post">';
             echo '<button type="submit" name="delete-entry" value=' . $shown_entry['eid'] . 'id="delete-entry">Lösche diesen Beitrag</button>';
             echo '</form>';
         }
+    }
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['delete-entry'])) {
             deleteEntry($entry['eid']);
@@ -57,12 +61,3 @@
     echo '</div>';
     echo '</div>';
 ?>
-
-<!--include "connection file";
-$query = "SELECT * FROM Blog";
-$result = mysqli_query($query);
-$num_results = mysqli_num_rows($result);
-for($i=0; $i<$num_results; $i++) {
-$row = mysqli_fetch_assoc($result);
-echo "<div class='blogEntry'><h4>" . $row['title'] . "</h4><h5>" . $row['date'] . "</h5><p>" . $row['text'] . "</p></div>";
-} -->
